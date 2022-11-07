160e7ab7de519275bcb1704994f7d32ad001ce61
PRA-1 <rajrpatel20@gnu.ac.in>
2022-11-07 21:34:23 +0530

    Initial commit

---
+++ app/src/main/java/com/example/practical_6_119/youtubeactivity.kt
@@ -0,0 +1,12 @@
+package com.example.practical_6_119
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class youtubeactivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_youtubeactivity)
+
+    }
+}
